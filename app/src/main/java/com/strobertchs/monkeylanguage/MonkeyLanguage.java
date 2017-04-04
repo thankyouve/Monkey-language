4f31e24f4a486e22f65ad0be57e8394e143cae46
Charles Zhang <Charles Zhang>
2017-04-03 22:28:19 -0400

    continue check words method

--- app/src/main/java/com/strobertchs/monkeylanguage/MonkeyLanguage.java
+++ app/src/main/java/com/strobertchs/monkeylanguage/MonkeyLanguage.java
@@ -2,12 +2,36 @@ package com.strobertchs.monkeylanguage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MonkeyLanguage extends AppCompatActivity {
+
+    String monkeyWordsList[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monkey_language);
     }
+
+    public void getWords (View v) {
+        EditText txtMonkeyWords = (EditText)findViewById(R.id.mtxtMonkeyWords);
+
+        TextView lblSimilations = (TextView)findViewById(R.id.similations);
+
+        String strMonkeyWords = txtMonkeyWords.getText().toString();
+
+        if (strMonkeyWords.equals("X")== false) {
+            monkeyWordsList = strMonkeyWords.split("\\n");
+        }
+        else {
+            lblSimilations.setText(checkMonkeyResult(monkeyWordsList);
+        }
+    }
+
+    private ArrayList<String> checkMonkeyResult (String MonkeyWordsList)
 }
