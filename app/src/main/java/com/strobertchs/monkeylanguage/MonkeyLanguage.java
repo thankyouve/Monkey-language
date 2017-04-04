1c30203c511d1a7e2e0b841121b7cf3df5405aea
Charles Zhang <Charles Zhang>
2017-04-04 18:16:11 -0400

    finish programming needs debug

--- app/src/main/java/com/strobertchs/monkeylanguage/MonkeyLanguage.java
+++ app/src/main/java/com/strobertchs/monkeylanguage/MonkeyLanguage.java
@@ -10,8 +10,6 @@ import java.util.ArrayList;
 
 public class MonkeyLanguage extends AppCompatActivity {
 
-    String monkeyWordsList[];
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,38 +19,86 @@ public class MonkeyLanguage extends AppCompatActivity {
     public void getWords (View v) {
         EditText txtMonkeyWords = (EditText)findViewById(R.id.mtxtMonkeyWords);
 
-        TextView lblSimilations = (TextView)findViewById(R.id.similations);
+        TextView outputResult = (TextView)findViewById(R.id.results);
 
         String strMonkeyWords = txtMonkeyWords.getText().toString();
 
-        if (strMonkeyWords.equals("X")== false) {
-            monkeyWordsList = strMonkeyWords.split("\\n");
-        }
-        else {
-            lblSimilations.setText(checkMonkeyResultStr(checkMonkeyResult(monkeyWordsList)));
-        }
+        String monkeyWordsList[] = strMonkeyWords.split("\\n");
+
+        outputResult.setText(checkMonkeyResultStr(checkMonkeyResult(monkeyWordsList)));
     }
 
-    private ArrayList<String> checkMonkeyResult(String MonkeyWordsList[]){
-        ArrayList<String> similations = new ArrayList<String>();
+    private ArrayList<String> checkMonkeyResult(String monkeyWordsList[]){
+        ArrayList<String> results = new ArrayList<>();
 
-        for (int i = 0; i < monkeyWordsList.length; i++){
+        for (int i = 0; i < monkeyWordsList.length; i++) {
             String firstCharacter = Character.toString(monkeyWordsList[i].charAt(0));
             String lastCharacter = Character.toString(monkeyWordsList[i].charAt(monkeyWordsList[i].length() - 1));
-            String twoCharacter = monkeyWordsList[i].substring(0,1);
-            if (firstCharacter.equals("A")){
-                if (lastCharacter.equals("A")){
-                    for (int m = 0;m < monkeyWordsList[i].length(); i += 2){
-                        if (twoCharacter.equals("AN")){
+            String secondLastChar = Character.toString(monkeyWordsList[i].charAt(monkeyWordsList[i].length() - 2));
 
+            if ((monkeyWordsList[i].length() == 1 && monkeyWordsList[i].equals("A") )||(monkeyWordsList[i].length() == 3 && monkeyWordsList[i].equals("BAS"))) {
+                results.add("YES");
+            }
+            else if (monkeyWordsList[i].length() % 2 == 1) {
+                if (firstCharacter.equals("A")) {
+                    if (lastCharacter.equals("A")) {
+                        for (int m = 0; m < monkeyWordsList[i].length() - 1; m += 2) {
+                            String twoCharacter = monkeyWordsList[i].substring(m, m + 1);
+
+                            if (twoCharacter.equals("AN")) {
+                                if (m == monkeyWordsList[i].length() - 2) {
+                                    results.add("YES");
+                                }
+                            }
+                            else {
+                                results.add("NO");
+                            }
                         }
                     }
+                    else {
+                        results.add("NO");
+                    }
+                }
+                else if (firstCharacter.equals("B")) {
+                    if (lastCharacter.equals("S")) {
+                        if (secondLastChar.equals("A")){
+                            for (int m = 1; m < monkeyWordsList[i].length() - 2; m += 2) {
+                                String twoCharacter = monkeyWordsList[i].substring(m, m + 1);
+
+                                if (twoCharacter.equals("AN")) {
+                                    if (m == monkeyWordsList[i].length() - 4) {
+                                        results.add("YES");
+                                    }
+                                }
+                                else {
+                                    results.add("NO");
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        results.add("NO");
+                    }
+                }
+                else{
+                    results.add("NO");
                 }
             }
+            else {
+                results.add("NO");
+            }
         }
+
+        return results;
     }
 
-    private String checkMonkeyResultStr(ArrayList<String> getWords){
+    private String checkMonkeyResultStr(ArrayList<String> resultsList){
+        String result = "";
 
+        for (String sentence: resultsList){
+            result = result + sentence + "\n";
+        }
+
+        return result;
     }
 }
