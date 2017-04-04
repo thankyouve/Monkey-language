13c0fbc1524b3acedb6b73d8693f60c29c3a5aab
Charles Zhang <Charles Zhang>
2017-04-03 21:31:15 -0400

    finish link main to MonkeyLanguage

--- app/src/main/java/com/strobertchs/monkeylanguage/MainActivity.java
+++ app/src/main/java/com/strobertchs/monkeylanguage/MainActivity.java
@@ -1,13 +1,30 @@
 package com.strobertchs.monkeylanguage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    String[] monkeyWords;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button buttonStart = (Button)findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        i = new Intent(this, MonkeyLanguage.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/strobertchs/monkeylanguage/MonkeyLanguage.java
@@ -0,0 +1,13 @@
+package com.strobertchs.monkeylanguage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MonkeyLanguage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monkey_language);
+    }
+}
