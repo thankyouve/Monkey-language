033b81abffdf6fa2f57b1d2d317f0bd8d94e7fe0
Charles Zhang <Charles Zhang>
2017-04-05 10:43:42 -0400

    learnt two words

--- app/src/main/java/com/strobertchs/monkeylanguage/MonkeyLanguage.java
+++ app/src/main/java/com/strobertchs/monkeylanguage/MonkeyLanguage.java
@@ -32,21 +32,24 @@ public class MonkeyLanguage extends AppCompatActivity{
         ArrayList<String> results = new ArrayList<>();
 
         for (int i = 0; i < monkeyWordsList.length; i++) {
-            String firstCharacter = Character.toString(monkeyWordsList[i].charAt(0));
-            String lastCharacter = Character.toString(monkeyWordsList[i].charAt(monkeyWordsList[i].length() - 1));
-            String secondLastChar = Character.toString(monkeyWordsList[i].charAt(monkeyWordsList[i].length() - 2));
-
-            if ((monkeyWordsList[i].length() == 1 && monkeyWordsList[i].equals("A") )||(monkeyWordsList[i].length() == 3 && monkeyWordsList[i].equals("BAS"))) {
+            if (monkeyWordsList[i].length() == 3 && monkeyWordsList[i].equals("BAS")) {
                 results.add("YES");
             }
-            else if (monkeyWordsList[i].length() % 2 == 1) {
+            else if (monkeyWordsList[i].length() == 1 && monkeyWordsList[i].equals("A")) {
+                results.add("YES");
+            }
+            else if (monkeyWordsList[i].length() % 2 == 1 && monkeyWordsList[i].length() != 1) {
+                String firstCharacter = Character.toString(monkeyWordsList[i].charAt(0));
+                String lastCharacter = Character.toString(monkeyWordsList[i].charAt(monkeyWordsList[i].length() - 1));
+                String secondLastChar = Character.toString(monkeyWordsList[i].charAt(monkeyWordsList[i].length() - 2));
+
                 if (firstCharacter.equals("A")) {
                     if (lastCharacter.equals("A")) {
-                        for (int m = 0; m < monkeyWordsList[i].length() - 1; m += 2) {
+                        for (int m = 0; m < (double)monkeyWordsList[i].length() - 0.5; m += 2) {
                             String twoCharacter = monkeyWordsList[i].substring(m, m + 1);
 
                             if (twoCharacter.equals("AN")) {
-                                if (m == monkeyWordsList[i].length() - 2) {
+                                if (m == monkeyWordsList[i].length() - 3) {
                                     results.add("YES");
                                 }
                             }
@@ -75,6 +78,9 @@ public class MonkeyLanguage extends AppCompatActivity{
                                 }
                             }
                         }
+                        else {
+                            results.add("NO");
+                        }
                     }
                     else {
                         results.add("NO");
@@ -92,7 +98,7 @@ public class MonkeyLanguage extends AppCompatActivity{
         return results;
     }
 
-    private String checkMonkeyResultStr(ArrayList<String> resultsList){
+    public String checkMonkeyResultStr(ArrayList<String> resultsList){
         String result = "";
 
         for (String sentence: resultsList){
